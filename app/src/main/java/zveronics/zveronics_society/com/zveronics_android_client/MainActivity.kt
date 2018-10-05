0be8c41e14f284cedf92631d2499def6b9fd1d95
RedSnail <Oleg.Demianchenko@gmail.com>
2018-10-05 16:00:20 +0300

    empty project (ne beite)

---
+++ app/src/main/java/zveronics/zveronics_society/com/zveronics_android_client/MainActivity.kt
@@ -0,0 +1,12 @@
+package zveronics.zveronics_society.com.zveronics_android_client
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
